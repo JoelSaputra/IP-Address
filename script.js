7a765b37651bfd5cffc553da49829e89d13f7154
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-06-12 19:51:33 -0400

    Finished implementing the leafletjs api

--- script.js
+++ script.js
@@ -8,7 +8,20 @@ const timezoneElement = document.getElementById('partition-timezone');
 const ispElement = document.getElementById('partition-isp');
 
 
+let map = null;
+let marker = null;
+
+let myIcon = L.icon({
+    iconUrl: 'images/icon-location.svg',
+    iconSize: [30, 45],
+    iconAnchor: [15, 45], 
+    popupAnchor: [0, -45]  
+});
+
+
+
 async function getIPLocation(ipAddress = null){
+
     let url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`;
 
     if(ipAddress && ipAddress.trim()!= ''){
@@ -18,12 +31,37 @@ async function getIPLocation(ipAddress = null){
     const response = await fetch(url);
     const data = await response.json();
 
+
     ipAddressElement.textContent = data.ip;
     locationElement.textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
     timezoneElement.textContent = `UTC${data.location.timezone}`;
     ispElement.textContent = data.isp;
 
+    let firstCoords = data.location.lat;
+    let secondCoords = data.location.lng;
+
+    if(!map){
+        
+        map = L.map('map').setView([firstCoords, secondCoords], 10);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        marker = L.marker([firstCoords, secondCoords], {icon: myIcon}).addTo(map);
+
+    }
+
+    else{
+        map.setView([firstCoords, secondCoords], 10);
+        marker.setLatLng([firstCoords, secondCoords]);
+
+    }
+
 }
+
+
 
 searchButton.addEventListener("click", ()=> {
     const searchTerm = searchInput.value.trim();
@@ -52,8 +90,10 @@ searchInput.addEventListener("keypress", (event)=>{
 
 });
  
-
 getIPLocation();
+
+
+
 
 
 
